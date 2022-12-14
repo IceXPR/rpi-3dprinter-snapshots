ee1bff63a00948bf6e2e6530868b0e78bcbb0e59
Julio <IceXPR@users.noreply.github.com>
2022-12-13 23:12:09 -0500

    fix cleanup directory

--- start-photo-snapshots.sh
+++ start-photo-snapshots.sh
@@ -3,7 +3,7 @@
 while true
 do
  mkdir -p public/printer-photos
- find . -name '*.*' -type f -mmin +1440 -delete
+ find public/printer-photos/ -name '*.*' -type f -mmin +1440 -delete
  raspistill -o public/printer-photos/printer-photo-`date +%H%M`.jpg
  sleep 300
 done
